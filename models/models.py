16236ff8c3aeaf36c80e49166ca9719596865b07
Felipe Peiro <felipe.peiro@grupodisber.es>
2017-05-25 09:39:50 +0200

    Second commit

--- models/models.py
+++ models/models.py
@@ -1,33 +1,40 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api, exceptions
 
+
 class Course(models.Model):
     _inherit = 'openacademy.course'
 
     #Add a new column to the openacademy.course model.
-    level = fields.Integer(string="Level", required=True, readonly=False)
+    level = fields.Integer(string="Level", required=True, default='0',
+                           readonly=False)
 
     @api.constrains('level')
     def validate_level(self):
         for rec in self:
-            if rec.level <= 0 or rec.level > 10:
+            if rec.level < 0 or rec.level > 10:
                 raise exceptions.ValidationError(
                     "Field value must be between 1 and 10.")
+
 
 class Partner(models.Model):
     _inherit = 'res.partner'
 
-    level = fields.Integer(string="Level", required=True, readonly=False)
+    level = fields.Integer(string="Level", required=True, default='0',
+                           readonly=False)
 
     @api.constrains('level')
     def _validate_level(self):
         for rec in self:
-            if rec.level <= 0 or rec.level > 10:
+            if rec.level < 0 or rec.level > 10:
                 raise exceptions.ValidationError(
                     "Field value must be between 1 and 10.")
 
+
 class Session(models.Model):
     _inherit = 'openacademy.session'
+
+    session_level = fields.Integer(related='course_id.level')
 
     @api.constrains('instructor_id')
     def _check_level_instructor(self):
